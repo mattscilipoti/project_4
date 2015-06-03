9bbb16995b28a7bc1194e989a5d918649eae014c
Aaron <aaronpark87@gmail.com>
2015-06-03 06:17:24 -0400

    posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,8 @@ class Post < ActiveRecord::Base
 	belongs_to :user 
 	has_many :sales, dependent: :destroy
 	# change to profile username later
-	def username
-    	username = self.user.email.split('@').first
+	def uid
+    	uid = self.user.email.split('@').first
  	end
 
 end
